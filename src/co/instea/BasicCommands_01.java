de5b0a6e050063ef223be56176d7b2fa09cf1d16
duris <duris.ivan@gmail.com>
2020-11-11 15:43:44 +0100

    5th commit, 2.issue from Jozef, added upgrade of

--- src/co/instea/BasicCommands_01.java
+++ src/co/instea/BasicCommands_01.java
@@ -1,89 +1,104 @@
 package co.instea;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class BasicCommands_01 {
-    public static void listOfCommands(){
-        System.out.println("Here is a list of commands");
-        System.out.println("* 'h' for help");
-        System.out.println("* 'q' to quit the program");
-        System.out.println("* 'set' to set read/write mode");
-    }
+
 
     public static void main(String[] args) throws IOException {
         System.out.println("Welcome to my console app, type 'h' to learn more.");
         Scanner input = new Scanner(System.in);
         Scanner console = new Scanner(System.in);
-        String letter = "";
+        String letter;
+        String letterTrim;
         String setModeFullText = "";
         //String setMode = "";
 
         //command line with neverending loop unless 'q' pressed for quit
         while(true) {
             letter = input.nextLine();
-            if (letter.equals("h")){
-                listOfCommands();
+            letterTrim = letter.trim();
+            if (letterTrim.equals("h")){
+                Settings_02.listOfCommands();
             }
-            else if (letter.equals("q")) {
+            else if (letterTrim.equals("q")) {
                 System.out.println("The console app will quit");
                 break;
             }
-            else if (letter.equals("set")) {
-                System.out.println("Set read / write mode. E.g. command to read from console and write into file  set -r 'console' -w 'file' ");
+            else if (letterTrim.equals("set") || letterTrim.contains("set " + "-r") || letterTrim.contains("set " + "-w")) {
+                System.out.println("Set read / write mode.");
+                System.out.println("E.g. command to read from console and write into file: 'set -r console -w file' ");
                 setModeFullText = input.nextLine();
                 String[] setMode = setModeFullText.split("\\s"); //splits the string based on whitespace
-                /*for (String s : setMode)
-                    System.out.println(s);
-                 */
+                boolean switcherRead=false;
+                boolean switcherWrite=false;
+                int rValue=0;
+                int wValue=0;
+                for (int i=0; i<setMode.length; i++){
+                    if (setMode[i].equals("-r")){
+                        switcherRead = true;
+                        rValue=i;
+                        //System.out.println("-r position: " + rValue);
+                    }
+                    else if (setMode[i].equals("-w")) {
+                        switcherWrite = true;
+                        wValue=i;
+                        //System.out.println("-w position: " + wValue);
+                    }
+                }
 
-                // reading mode
-                if (setMode[1].equals("-r") ){
-                    System.out.println("--- Command for reading ");
-                    System.out.println("'set -r file'    OR      'set -r console' ?");
-                    setModeFullText = input.nextLine();
-                    if (setModeFullText.contains("set -r console")){
-                        System.out.println("Enter text to be read from");
-                        setModeFullText = input.nextLine();
-                        System.out.println(setModeFullText);
+                if(switcherRead==true && switcherWrite==true){
+                    if(setMode[rValue+1].equals("file") && setMode[wValue+1].equals("file")){
+                        System.out.println("ToDo> Setup for reading from file and writing into file");
+                        continue;
                     }
-                    else if(setModeFullText.contains("set -r file")){
-                        String[] lines = Settings_02.readFromFile("c:/Users/duris/IdeaProjects/GitHubProject/inputFile.txt");
-                        System.out.println("Following text was read out of the file");
-                        for (String line : lines)
-                            System.out.println(line);
+                    else if(setMode[rValue+1].equals("file")&& setMode[wValue+1].equals("console")){
+                        System.out.println("ToDo> Setup for reading from file and writing into console");
+                        continue;
                     }
-                }
-                else if(setMode[1].equals("-w")){
-                    System.out.println("--- Command for writing ");
-                    System.out.println("'set -w file'    OR      'set -w console' ?");
-                    setModeFullText = input.nextLine();
-                    if (setModeFullText.contains("set -w console")){
-                        System.out.println("Enter text to be written on console");
-                        setModeFullText = input.nextLine();
-                        System.out.println(setModeFullText);
+                    else if(setMode[rValue+1].equals("console")&& setMode[wValue+1].equals("file")){
+                        System.out.println("ToDo> Setup for reading from console and writing into file");
+                        continue;
                     }
-                    else if(setModeFullText.contains("set -w file")){
-                        System.out.println("Enter text to be written into file");
-                        setModeFullText = input.nextLine();
-                        Settings_02.writingIntoFile("c:/Users/duris/IdeaProjects/GitHubProject/outputFile.txt", setModeFullText);
+                    else if(setMode[rValue+1].equals("console")&& setMode[wValue+1].equals("console")){
+                        System.out.println("ToDo> Setup for reading from console and writing into console");
+                        continue;
                     }
-
+                    else
+                        continue;
                 }
-                else if (letter.equals("h")){
-                    listOfCommands();
+                else if(switcherRead==true&& switcherWrite==false){
+                    if(setMode[rValue+1].equals("file")){
+                        System.out.println("ToDo> Setup for reading from file");
+                        continue;
+                    }
+                    else if(setMode[rValue+1].equals("console")){
+                        System.out.println("ToDo> Setup for reading from console");
+                        continue;
+                    }
+                    else
+                        continue;
                 }
-                else if (letter.equals("q")) {
-                    System.out.println("The console app will quit");
-                    break;
+                else if(switcherRead==false&& switcherWrite==true){
+                    if(setMode[wValue+1].equals("file")){
+                        System.out.println("ToDo> Setup for writing into file");
+                        continue;
+                    }
+                    else if(setMode[wValue+1].equals("console")){
+                        System.out.println("ToDo> Setup for writing to console");
+                        continue;
+                    }
+                    else
+                        continue;
                 }
-                else {
-                    System.out.println("Wrong set command... Now you will returned to 'Set read / write' mode again");
+                else
                     continue;
-                }
             }
             else
                 System.out.println("This is not a valid command");
+                continue;
         }
 
     }
