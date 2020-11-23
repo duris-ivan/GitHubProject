57e995ae69dc91a55e991821e2c93c3c827e60f7
duris <duris.ivan@gmail.com>
2020-11-23 11:56:02 +0100

    add Settings.java

---
+++ src/co/instea/Settings.java
@@ -0,0 +1,32 @@
+package co.instea;
+
+import java.util.Set;
+
+public class Settings {
+    private String inputType;
+    private String outputType;
+
+    public Settings(){
+    }
+
+    public Settings(String inputType, String outputType){
+        this.inputType=inputType;
+        this.outputType=outputType;
+    }
+
+    public String getInputType() {
+        return inputType;
+    }
+
+    public String getOutputType() {
+        return outputType;
+    }
+
+    public void setInputType(String inputType) {
+        this.inputType = inputType;
+    }
+
+    public void setOutputType(String outputType) {
+        this.outputType = outputType;
+    }
+}
